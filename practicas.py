3bc516836217aa741810b9e955903817dc2bcfb1
andrea <elianiscervantessanchez@gmail.com>
2026-02-26 20:48:47 -0500

    punto

--- practicas.py
+++ practicas.py
@@ -5,7 +5,7 @@
 # elif numero > 0:
 #     print("el numero es positivo")
 # else:
-#     print("el numero es negativo")
+# .print("el numero es negativo")
 
 
 
@@ -14,5 +14,5 @@ age = int(input("digite su edad: "))
 if age >= 18:
     print("eres mayor de edad")
 else: 
-     print("eres menor de edad")
+     print("eres menor de edd")
 
