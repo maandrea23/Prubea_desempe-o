ffd6f6434293c8c0a223a854f7151a69e0c89beb
elianis20 <elianiscervantessanchez@gmail.com>
2026-02-26 20:31:37 -0500

    descomente el codigo

---
+++ practicas.py
@@ -0,0 +1,18 @@
+# numero = int(input("Digite un numero: "))
+
+# if numero == 0:
+#     print("el numero es cero")
+# elif numero > 0:
+#     print("el numero es positivo")
+# else:
+#     print("el numero es negativo")
+
+
+
+age = int(input("digite su edad: "))
+
+if age >= 18:
+    print("eres mayor de edad")
+else: 
+     print("eres menor de edad")
+
