4abe7d3ebc445bc8a5d71a7aa2378df1c9dfa9c2
andrea <>
2026-03-10 20:54:36 -0500

    continue

--- WEEK_1/list_and_dic.py
+++ WEEK_1/list_and_dic.py
@@ -1,3 +1,4 @@
+# 1. Agregar y eliminar elementos de una lista
 list = ["manzana", "pera", "naranja", "uva", "fresa"]
 list.append("kiwi")
 print(list)
@@ -7,3 +8,18 @@ list.remove("uva")
 print(list)
 list.pop()
 print(list)
+
+# 2. Ordenar y contar elementos en una lista
+numeros = [4, 2, 7, 2, 9, 1, 2]
+numeros.sort()
+print(f"Lista usando sort: {numeros}")
+numeros.reverse()
+print(numeros)
+numeros.count(2)
+print(numeros.count(2))
+numeros.index(7)
+print(numeros.index(7))
+
+# 3. Copiar y extender listas
+a = [1, 2, 3]
+b = [4, 5, 6]
